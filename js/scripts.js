cda57b7ec08999e1040764493d0d5caa3dc1b003
Chloe ONeil <chloesporri@yahoo.com>
2022-10-31 10:46:53 -0700

    change innerText to reflect input

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
+
     const person1Input= document.getElementById("person1Input").value;
     const person2Input= document.getElementById("person2Input").value;
     const animalInput= document.getElementById("animalInput").value;
@@ -8,15 +9,15 @@ window.onload = function() {
     const verbInput= document.getElementById("verbInput").value;
     const nounInput= document.getElementById("nounInput").value;
 
-    document.querySelector("span#person1a").innerText= "person 1";
-    document.querySelector("span#person1b").innerText= "person 1";
-    document.querySelector("span#person1c").innerText= "person 1";
-    document.querySelector("span#person2a").innerText= "person 2";
-    document.querySelector("span#person2b").innerText= "person 2";
-    document.querySelector("span#animal").innerText= "an animal";
-    document.querySelector("sapn#verb").innerText= "verb";
-    document.querySelector("span#noun").innerText= "noun";
-    document.querySelector("span#exclamation").innerText= "exclamation";
+    document.querySelector("span#person1a").innerText= person1Input;
+    document.querySelector("span#person1b").innerText= person1Input;
+    document.querySelector("span#person1c").innerText= person1Input;
+    document.querySelector("span#person2a").innerText= person2Input;
+    document.querySelector("span#person2b").innerText= person2Input;
+    document.querySelector("span#animal").innerText= animalInput;
+    document.querySelector("sapn#verb").innerText= verbInput;
+    document.querySelector("span#noun").innerText= nounInput;
+    document.querySelector("span#exclamation").innerText= exclamationInput;
 
     document.querySelector("div#story").removeAttribute("class");
 
