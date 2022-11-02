c2b3366ee4d469f44fedb69423472b6b66c319a8
Chloe ONeil <chloesporri@yahoo.com>
2022-11-01 17:32:01 -0700

    add pop up and onEvents for three seperate scenarios

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 window.addEventListener("load", function() {
   let form = document.querySelector("form");
-  form.addEventListener("submit", function(event) {
+  let resetBtn= document.getElementById("reset");
+  let story= document.getElementById("story");
 
-    const person1Input= document.getElementById("person1Input").value;
-    const person2Input= document.getElementById("person2Input").value;
+  form.addEventListener("submit", function(event) {
+    const person1Input = document.getElementById("person1Input").value;
+    const person2Input = document.getElementById("person2Input").value;
     const animalInput= document.getElementById("animalInput").value;
-    const exclamationInput= document.getElementById("exclamationInput").value;
-    const verbInput= document.getElementById("verbInput").value;
-    const nounInput= document.getElementById("nounInput").value;
+    const exclamationInput = document.getElementById("exclamationInput").value;
+    const verbInput = document.getElementById("verbInput").value;
+    const nounInput = document.getElementById("nounInput").value;
 
     document.querySelector("span#person1a").innerText= person1Input;
     document.querySelector("span#person1b").innerText= person1Input;
@@ -19,10 +21,27 @@ window.addEventListener("load", function() {
     document.querySelector("span#noun").innerText= nounInput;
     document.querySelector("span#exclamation").innerText= exclamationInput;
 
-    document.querySelector("div#story").removeAttribute("class");
-
+    story.removeAttribute("class");
     event.preventDefault();
-    });
+  });
+
+  form.addEventListener("submit", function() {
+    resetBtn.removeAttribute("class");
+  });
+
+  form.addEventListener("submit", function() {
+  window.alert("Do you need a new computer? Visit www.superextracomputersales.com to find the best deals!");
+  });
+
+  resetBtn.addEventListener("click", function() {
+    story.setAttribute("class", "hidden");
+    document.getElementById("person1Input").value=null;
+    document.getElementById("person2Input").value=null;
+    document.getElementById("animalInput").value=null;
+    document.getElementById("exclamationInput").value=null;
+    document.getElementById("verbInput").value=null;
+    document.getElementById("nounInput").value=null;
+  });
 });
 
 
